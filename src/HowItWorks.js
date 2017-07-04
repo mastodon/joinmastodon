decb2b942cd83f0310e16bb515277ab5a62f5ca2
Eugen Rochko <eugen@zeonfederated.com>
2017-07-05 00:03:29 +0200

    Add elephants graphic

--- src/HowItWorks.js
+++ src/HowItWorks.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
+import elephantGrid from './assets/instance_badges.png';
+
 const HowItWorks = () => (
   <div className='how-it-works' id='how-it-works'>
     <div className='container'>
-      <h2>How it works</h2>
+      <div className='row'>
+        <div className='specs'>
+          <h2>How it works</h2>
 
-      <div className='specs'>
-        <p>Anyone can run a server of Mastodon. Each server hosts individual user accounts, the content they produce, and the content they subscribe to.</p>
-        <p>Each user account has a globally unique name (e.g. @user@example.com), consisting of the local username (@user), and the domain name of the server it is on (example.com).</p>
-        <p>Users can follow each other, regardless of where they're hosted &mdash; when a local user follows a user from a different server, the server subscribes to that user's updates for the first time.</p>
+          <p>Anyone can run a server of Mastodon. Each server hosts individual user accounts, the content they produce, and the content they subscribe to.</p>
+          <p>Each user account has a globally unique name (e.g. @user@example.com), consisting of the local username (@user), and the domain name of the server it is on (example.com).</p>
+          <p>Users can follow each other, regardless of where they're hosted &mdash; when a local user follows a user from a different server, the server subscribes to that user's updates for the first time.</p>
+        </div>
+
+        <div className='network-grid'>
+          <img src={elephantGrid} alt='Fediverse' />
+        </div>
       </div>
 
       <div className='benefits'>
