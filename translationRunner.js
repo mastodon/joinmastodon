43092cf88aee19a215ce15a80b1fbd3074d59fe2
Fernando Mumbach (fermuch) <fermuch@gmail.com>
2017-09-16 18:41:12 -0300

    added es.json (testing; only one string translated)

--- translationRunner.js
+++ translationRunner.js
@@ -3,5 +3,5 @@ const manageTranslations = require('react-intl-translations-manager').default;
 manageTranslations({
   messagesDirectory: 'build/messages',
   translationsDirectory: 'src/locales/',
-  languages: ['en']
+  languages: ['en', 'es']
 });
