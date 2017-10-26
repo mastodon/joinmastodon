fda88f756ea400e3853a3e2f19f0b3666f96fb48
Alexander Yakovlev <keloero@oreolek.ru>
2017-10-26 13:30:50 +0700

    restored missing line

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import de from 'react-intl/locale-data/de';
 import ja from 'react-intl/locale-data/ja';
 import pt from 'react-intl/locale-data/pt';
 import zh from 'react-intl/locale-data/zh';
+import ru from 'react-intl/locale-data/ru';
 
 addLocaleData([...en, ...pl, ...fr, ...es, ...ja, ...de, ...pt, ...zh, ...ru]);
 
