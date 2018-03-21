2086319585d28b8465e0f2e8ce9f99c449c52392
Eugen Rochko <eugen@zeonfederated.com>
2018-03-21 18:41:29 +0100

    Move items in navigation to dropdown, link blog and merch

--- src/LanguageSelect.js
+++ src/LanguageSelect.js
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react';
-import classNames from 'classnames';
+import React from 'react';
+import Dropdown from './Dropdown';
 
 import flagEn from 'twemoji/2/svg/1f1ec-1f1e7.svg';
 import flagFr from 'twemoji/2/svg/1f1eb-1f1f7.svg';
@@ -27,62 +27,11 @@ const options = [
   { value: 'ar', label: <span><img src={flagAr} className='emoji' alt='' /> العربية</span> },
 ];
 
-export default class LanguageSelect extends PureComponent {
-  state = {
-    opened: false
-  };
-
-  renderMenu () {
-    const { value } = this.props;
-
-    return (
-      <div className='dropdown__menu'>
-        {options.map(option => (
-          <div key={option.value} className={classNames('dropdown__option', { active: option.value === value })} onClick={this.handleOptionClick.bind(this, option.value)}>
-            {option.label}
-          </div>
-        ))}
-      </div>
-    );
-  }
-
-  handleOptionClick = (value) => {
-    this.props.onChange(value);
-    this.setState({ opened: false });
-  }
-
-  handleClick = () => {
-    this.setState({ opened: !this.state.opened });
-  }
-
-  handleDocumentClick = e => {
-    if (this.node && !this.node.contains(e.target)) {
-      this.setState({ opened: false });
-    }
-  }
-
-  componentDidMount () {
-    document.addEventListener('click', this.handleDocumentClick, false);
-    document.addEventListener('touchend', this.handleDocumentClick, false);
-  }
-
-  componentWillUnmount () {
-    document.removeEventListener('click', this.handleDocumentClick, false);
-    document.removeEventListener('touchend', this.handleDocumentClick, false);
-  }
-
-  setRef = c => {
-    this.node = c;
-  }
-
-  render () {
-    const { opened } = this.state;
-
-    return (
-      <div className={classNames('dropdown', { active: opened })} ref={this.setRef}>
-        <div className='dropdown__control' tabIndex='0' role='button' onClick={this.handleClick}>文A <i className='dropdown__arrow ion-ios-arrow-down' /></div>
-        {opened && this.renderMenu()}
-      </div>
-    );
-  }
-}
+export default ({ value, onChange }) => (
+  <Dropdown
+    options={options}
+    value={value}
+    label='文A'
+    onChange={onChange}
+  />
+);
