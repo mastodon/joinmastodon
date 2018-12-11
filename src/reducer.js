0e20df496ac6d764f831584c576d12f596304b56
Eugen Rochko <eugen@zeonfederated.com>
2018-12-11 04:38:19 +0100

    Update src/reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -28,12 +28,17 @@ const initialState = {
   },
 };
 
+const blacklist = [
+  'mastodon.social',
+  'abdl.link',
+];
+
 export default function reducer(state = initialState, action) {
   switch(action.type) {
   case LOCALE_CHANGE:
     return { ...state, locale: action.data };
   case INSTANCES_FETCH_SUCCESS:
-    return { ...state, instances: action.data.filter(instance => instance.name !== 'mastodon.social') };
+    return { ...state, instances: action.data.filter(instance => blacklist.indexOf(instance.name) === -1) };
   case FILTER_CATEGORY_CHANGE:
     return { ...state, filter: { ...state.filter, category: action.data } };
   case FILTER_LANGUAGE_CHANGE:
