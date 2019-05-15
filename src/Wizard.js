cb6ae2ab5e126bbc7dacc2aff4d57e5fd883c1ad
Eugen Rochko <eugen@zeonfederated.com>
2019-05-15 23:45:43 +0200

    Re-add activism category

--- src/Wizard.js
+++ src/Wizard.js
@@ -110,7 +110,7 @@ class Wizard extends React.PureComponent {
                 //{ value: 'music', text: intl.formatMessage(messages.music) },
                 //{ value: 'books', text: intl.formatMessage(messages.books) },
                 //{ value: 'journalism', text: intl.formatMessage(messages.journalism) },
-                //{ value: 'activism', text: intl.formatMessage(messages.activism) },
+                { value: 'activism', text: intl.formatMessage(messages.activism) },
                 //{ value: 'lgbt', text: intl.formatMessage(messages.lgbt) },
                 //{ value: 'poc', text: intl.formatMessage(messages.poc) },
                 //{ value: 'sports', text: intl.formatMessage(messages.sports) },
