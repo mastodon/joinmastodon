906c3bb7328f0c658333af0b4ef6333dd8feacc9
Eugen Rochko <eugen@zeonfederated.com>
2020-08-19 23:14:48 +0200

    Update configurations

--- translationRunner.js
+++ translationRunner.js
@@ -1,34 +1,8 @@
 const manageTranslations = require('react-intl-translations-manager').default;
+const locales = require('./src/LanguageSelect').options.map(x => x.value);
 
 manageTranslations({
   messagesDirectory: 'build/messages',
   translationsDirectory: 'src/locales/',
-  languages: [
-    'ar',
-    'ca',
-    'cs',
-    'cy',
-    'de',
-    'en',
-    'es',
-    'eu',
-    'fi',
-    'fr',
-    'he',
-    'it',
-    'ja',
-    'ko',
-    'nl-NL',
-    'no',
-    'pl',
-    'pt-BR',
-    'ru',
-    'sk',
-    'sq',
-    'sv',
-    'tr',
-    'uk',
-    'zh',
-    'zh-TW',
-  ]
+  languages: locales,
 });
