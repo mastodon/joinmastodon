17dcb9fe52888a91bfc82c3cb31e5373aa5cd4e0
Eugen Rochko <eugen@zeonfederated.com>
2020-09-29 01:06:10 +0200

    Update src/Sponsorship.js

--- src/Sponsorship.js
+++ src/Sponsorship.js
@@ -191,7 +191,6 @@ const Sponsorship = ({ intl }) => (
               <li>Alice Bloom</li>
               <li>ThePhotoApp</li>
               <li>Ivan Braun</li>
-              <li>Sergey</li>
             </ul>
 
             <ul>
