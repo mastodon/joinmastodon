42d2dfd7ee243d6bf1e993213cf3cd26e9077ee2
Eugen Rochko <eugen@zeonfederated.com>
2021-02-20 23:58:46 +0100

    Update src/assets/sponsors/KEVANI_LOGO.png, src/Sponsorship.js files

--- src/Sponsorship.js
+++ src/Sponsorship.js
@@ -20,6 +20,7 @@ import sponsorCocosign from './assets/sponsors/cocosign2.png';
 import sponsorQuarterless from './assets/sponsors/quarterless.png';
 import sponsorNextiva from './assets/sponsors/nextiva.svg';
 import sponsorSchnellNode from './assets/sponsors/schnellnode.png';
+import sponsorKevani from './assets/sponsors/KEVANI_LOGO.png';
 
 import noAvatar from './assets/sponsors/32x32.png';
 import avatarEvan from './assets/sponsors/evan.png';
@@ -65,6 +66,7 @@ const goldSponsors = [
   { href: 'https://www.nextiva.com/', src: sponsorNextiva, alt: 'Nextiva' },
   { href: 'https://quarterless.com/', src: sponsorQuarterless, alt: 'Quarterless' }, // Custom
   { href: 'https://schnellno.de/', src: sponsorSchnellNode, alt: 'Schnell Node' },
+  { href: 'https://www.kevani.com/', src: sponsorKevani, alt: 'Kevani' },
 ];
 
 const silverSponsors = [
