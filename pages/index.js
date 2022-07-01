e450a4a9e08990124d8c426595480ed15df79125
Mika Busante <mika@mikabusante.com>
2022-07-01 15:17:15 -0400

    load headline on home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
-function Home() {
+import { FormattedMessage } from "react-intl"
+import loadIntlMessages from "../utils/loadIntlMessages"
+
+function Home(props) {
   return (
     <div>
-      <h1>Home page placeholder</h1>
+      <h1>
+        <FormattedMessage
+          id="home.headline"
+          defaultMessage="Social networking that's not for sale."
+        />
+      </h1>
     </div>
   )
 }
 
 export default Home
+
+export async function getStaticProps(ctx) {
+  return {
+    props: { intlMessages: await loadIntlMessages(ctx) },
+  }
+}
