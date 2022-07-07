33a9688eb4a807ad20cfad61ff6f18faec7b2a42
Mika Busante <mika@mikabusante.com>
2022-07-07 15:36:33 -0400

    scaffold out home page hero

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,72 @@
 import { FormattedMessage } from "react-intl"
 import loadIntlMessages from "../utils/loadIntlMessages"
+import LinkButton from "../components/LinkButton"
 
 function Home() {
+  return <HomeHero />
+}
+
+export default Home
+
+function HomeHero() {
   return (
-    <div className="grid">
-      <h1 className="col-center-10 text-center h1">
+    <section className="grid hero">
+      <h1 className="h1">
         <FormattedMessage
           id="home.hero.headline"
           defaultMessage="Social networking that's not for sale."
         />
       </h1>
-    </div>
+
+      <p className="sh1">
+        <FormattedMessage
+          id="home.hero.body"
+          defaultMessage="Your home feed should be filled with what matters to you most, not what a corporation thinks you should see. Radically different social media, back in the hands of the people."
+        />
+      </p>
+
+      <div className="hero__button-row">
+        <LinkButton href="/">
+          <FormattedMessage
+            id="home.how_it_works"
+            defaultMessage="How it works"
+          />
+        </LinkButton>
+
+        <LinkButton href="/" light>
+          <FormattedMessage
+            id="home.get_started"
+            defaultMessage="Get started"
+          />
+        </LinkButton>
+      </div>
+      <style jsx>{`
+        h1 {
+          max-width: 15ch;
+        }
+
+        p {
+          max-width: 45ch;
+        }
+
+        h1,
+        p {
+          justify-self: center;
+        }
+
+        .hero {
+          text-align: center;
+        }
+
+        .hero__button-row {
+          display: flex;
+          gap: 3rem;
+          justify-content: center;
+        }
+      `}</style>
+    </section>
   )
 }
-
-export default Home
 
 export async function getStaticProps(ctx) {
   return {
