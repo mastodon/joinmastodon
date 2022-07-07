8935c1d7d9a886f233cbfe8872f896b211428341
Mika Busante <mika@mikabusante.com>
2022-07-07 11:28:49 -0400

    add a few util classes

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,28 @@ import loadIntlMessages from "../utils/loadIntlMessages"
 
 function Home() {
   return (
-    <div>
-      <h1>
+    <div className="grid">
+      <h1 className="col-center-10 text-center h1">
         <FormattedMessage
           id="home.hero.headline"
           defaultMessage="Social networking that's not for sale."
         />
       </h1>
+
+      <div className="grid">
+        <h2>This is an inner grid</h2>
+        <div class="col-span-4">Spanning 4 columns</div>
+        <div class="col-span-4">Spanning 4 columns</div>
+        <div class="col-span-4">Spanning 4 columns</div>
+
+        <div className="grid">
+          <h2>This is an inner inner grid</h2>
+          <div class="col-span-3">Spanning 3 columns</div>
+          <div class="col-span-3">Spanning 3 columns</div>
+          <div class="col-span-3">Spanning 3 columns</div>
+          <div class="col-span-3">Spanning 3 columns</div>
+        </div>
+      </div>
     </div>
   )
 }
