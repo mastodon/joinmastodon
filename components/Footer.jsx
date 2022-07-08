10ce588babe94d2c91f15a708ecdda26948bfe73
Daniel Gamage <hello@danielgamage.com>
2022-07-08 17:06:32 -0400

    footer in tailwind

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,70 +6,69 @@ import Image from "next/image"
 
 export const Footer = () => (
   <>
-    <footer className="Footer">
-      <nav className="container">
-        <div className="Footer__grid grid">
-          <div className="Footer__logo">
+    <footer className="relative text-white pb-24 md:pt-24 mt-72 text-center md:text-left">
+      <div className="full-width-bg bg-nightshade">
+        <div className="absolute bottom-full left-1/2 -translate-x-1/2 w-full h-48 md:h-72 translate-y-[1px] [background-position:90%_bottom] [background-size:MAX(64rem,100vw)_auto] bg-no-repeat bg-[url(/illustrations/swoosh.svg)]"></div>
+      </div>
+      <nav>
+        <div className="grid gap-x-4 md:gap-x-5 gap-y-16 md:grid-cols-12">
+          <div className="justify-self-center max-w-full w-20 md:col-start-1 md:col-end-4 lg:col-start-2 lg:col-end-5">
             <Image
               src={mastodonLogo}
               alt="Mastodon"
               className="footer-grid__brand"
             />
           </div>
-          <div className="Footer__menus">
-            <div className="Footer__menu">
-              <h4 className="Footer__menu-heading h6">
-                <FormattedMessage id="nav.product" defaultMessage="Product" />
-              </h4>
-              <ul className="b2">
-                <li>
-                  <Link href="/communities">
-                    <a>
+          <div className="grid gap-10 md:grid-cols-3 gap-x-4 md:col-start-4 md:col-end-13 md:gap-x-5 lg:col-start-6 lg:col-end-12" >
+            {[
+              {
+                heading: (
+                  <FormattedMessage id="nav.product" defaultMessage="Product" />
+                ),
+                links: [
+                  <Link key="communities" href="/communities">
+                    <a className="py-2 inline-block">
                       <FormattedMessage
                         id="home.get_started"
                         defaultMessage="Get started"
                       />
                     </a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/apps">
-                    <a>
+                  </Link>,
+                  <Link key="apps" href="/apps">
+                    <a className="py-2 inline-block">
                       <FormattedMessage id="nav.apps" defaultMessage="Apps" />
                     </a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/sponsors">
-                    <a>
+                  </Link>,
+                  <Link key="sponsors" href="/sponsors">
+                    <a className="py-2 inline-block">
                       <FormattedMessage
                         id="nav.sponsors"
                         defaultMessage="Sponsors"
                       />
                     </a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="Footer__menu">
-              <h4 className="Footer__menu-heading h6">
-                <FormattedMessage
-                  id="nav.resources"
-                  defaultMessage="Resources"
-                />
-              </h4>
-              <ul className="b2">
-                <li>
+                  </Link>,
+                ],
+              },
+              {
+                heading: (
+                  <FormattedMessage
+                    id="nav.resources"
+                    defaultMessage="Resources"
+                  />
+                ),
+                links: [
                   <a
+                    className="py-2 inline-block"
+                    key="mastodon/mastodon"
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://github.com/mastodon/mastodon"
                   >
                     <FormattedMessage id="nav.code" defaultMessage="Code" />
-                  </a>
-                </li>
-                <li>
+                  </a>,
                   <a
+                    className="py-2 inline-block"
+                    key="joinmastodon.org"
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://docs.joinmastodon.org"
@@ -78,10 +77,10 @@ export const Footer = () => (
                       id="nav.docs"
                       defaultMessage="Documentation"
                     />
-                  </a>
-                </li>
-                <li>
+                  </a>,
                   <a
+                    className="py-2 inline-block"
+                    key="mastodon/discussions"
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://github.com/mastodon/mastodon/discussions"
@@ -90,129 +89,65 @@ export const Footer = () => (
                       id="nav.support"
                       defaultMessage="Support"
                     />
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="Footer__menu">
-              <h4 className="Footer__menu-heading h6">
-                <FormattedMessage id="nav.company" defaultMessage="Company" />
-              </h4>
-              <ul className="b2">
-                <li>
-                  <Link href="/imprint">
-                    <a>
+                  </a>,
+                ],
+              },
+              {
+                heading: (
+                  <FormattedMessage id="nav.company" defaultMessage="Company" />
+                ),
+                links: [
+                  <Link key="/imprint" href="/imprint">
+                    <a className="py-2 inline-block">
                       <FormattedMessage
                         id="contact_us"
                         defaultMessage="Contact us"
                       />
                     </a>
-                  </Link>
-                </li>
-                <li>
+                  </Link>,
                   <a
+                    className="py-2 inline-block"
                     target="_blank"
                     rel="noopener noreferrer"
+                    key="https://blog.joinmastodon.org"
                     href="https://blog.joinmastodon.org/"
                   >
                     <FormattedMessage id="nav.blog" defaultMessage="Blog" />
-                  </a>
-                </li>
-                <li>
-                  <a href="/press-kit.zip">
+                  </a>,
+                  <a
+                    className="py-2 inline-block"
+                    key="/press-kit.zip"
+                    href="/press-kit.zip"
+                  >
                     <FormattedMessage
                       id="credits.press_kit"
                       defaultMessage="Press kit"
                     />
-                  </a>
-                </li>
-                <li>
-                  <Link href="/privacy-policy">
-                    <a>
+                  </a>,
+                  <Link key="/privacy-policy" href="/privacy-policy">
+                    <a className="py-2 inline-block">
                       <FormattedMessage
                         id="privacy_policy"
                         defaultMessage="Privacy policy"
                       />
                     </a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
+                  </Link>,
+                ],
+              },
+            ].map((menu) => (
+              <div className="flex flex-col gap-2" key={menu.heading}>
+                <h4 className="py-1 h6">{menu.heading}</h4>
+                <ul className="b2 flex flex-col m-0 p-0 gap-2">
+                  {menu.links.map((link) => (
+                    <li key={link.key}>{link}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
           </div>
         </div>
       </nav>
     </footer>
-    <style jsx>{`
-      .Footer {
-        background: var(--eggplant);
-        color: var(--white);
-        padding: 6rem 0;
-        text-align: center;
-      }
-      .Footer__menu-heading {
-        line-height: 1.5rem;
-        padding: 0.5rem 0;
-      }
-      .Footer__grid {
-        row-gap: 2.5rem;
-      }
-      .Footer__menu ul {
-        display: flex;
-        flex-direction: column;
-        gap: 0.5rem;
-        list-style: none;
-        padding: 0;
-        margin: 0;
-      }
-      .Footer__menus {
-        display: grid;
-        gap: 2.5rem;
-      }
-      .Footer__menu {
-        display: flex;
-        flex-direction: column;
-        gap: 0.5rem;
-      }
-      .Footer__menu a {
-        display: inline-block;
-        padding-block: 0.5rem;
-      }
-      .Footer__logo {
-        width: 5.5rem;
-        max-width: 100%;
-        justify-self: center;
-      }
-      @media (min-width: 576px) {
-        .Footer {
-          text-align: left;
-        }
-        .Footer__logo {
-          grid-column: 1 / 2;
-          justify-self: center;
-        }
-        .Footer__menus {
-          grid-column: 2 / 5;
-          grid-template-columns: repeat(3, 1fr);
-          gap: var(--gutter-width);
-        }
-      }
-      @media (min-width: 768px) {
-        .Footer__logo {
-          grid-column: 1 / 4;
-        }
-        .Footer__menus {
-          grid-column: 4 / 13;
-        }
-      }
-      @media (min-width: 992px) {
-        .Footer__logo {
-          grid-column: 2 / 5;
-        }
-        .Footer__menus {
-          grid-column: 6 / 12;
-        }
-      }
-    `}</style>
   </>
 )
 
