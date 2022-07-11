6e7f1e30aae956964fb414a138f0a570b562f179
Mika Busante <mika@mikabusante.com>
2022-07-11 11:46:05 -0400

    add `target` and `rel` attributes for external URLs

--- components/LinkButton.jsx
+++ components/LinkButton.jsx
@@ -2,6 +2,14 @@ import Link from "next/link"
 import classnames from "classnames"
 
 const LinkButton = ({ borderless, children, fullWidth, href, light }) => {
+  const isExternalURL = (url) => {
+    try {
+      return new URL(url).origin !== location.origin
+    } catch (_) {
+      return false
+    }
+  }
+
   return (
     <Link href={href}>
       <a
@@ -14,6 +22,8 @@ const LinkButton = ({ borderless, children, fullWidth, href, light }) => {
             "bg-white text-accent-blurple hover:text-white": light,
           }
         )}
+        target={isExternalURL(href) && "_blank"}
+        rel={isExternalURL(href) && "noopener noreferrer"}
       >
         {children}
       </a>
