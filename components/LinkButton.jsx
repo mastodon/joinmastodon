f333b836ddf2db2366aafc298a2cc70a8e6ad62f
Mika Busante <mika@mikabusante.com>
2022-07-11 11:57:38 -0400

    add important rule, was getting overridden by `w-max`
    (because the class names are arranged alphabetically)

--- components/LinkButton.jsx
+++ components/LinkButton.jsx
@@ -17,7 +17,7 @@ const LinkButton = ({ borderless, children, fullWidth, href, light }) => {
           "b3 block w-max rounded border-2 border-accent-blurple  p-4 !font-600  transition-all hover:border-dark-blurple hover:bg-dark-blurple",
           {
             "border-white": borderless,
-            "w-full": fullWidth,
+            "!w-full": fullWidth,
             "bg-accent-blurple text-white": !light,
             "bg-white text-accent-blurple hover:text-white": light,
           }
