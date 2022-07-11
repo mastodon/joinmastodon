0f5a167c3b958c8caa876d4befe8ad781b7a94c3
Mika Busante <mika@mikabusante.com>
2022-07-11 12:04:24 -0400

    try to improve readability
    using ternary statements to switch between classes instead of overriding.

--- components/LinkButton.jsx
+++ components/LinkButton.jsx
@@ -14,13 +14,12 @@ const LinkButton = ({ borderless, children, fullWidth, href, light }) => {
     <Link href={href}>
       <a
         className={classnames(
-          "b3 block w-max rounded border-2 border-accent-blurple  p-4 !font-600  transition-all hover:border-dark-blurple hover:bg-dark-blurple",
-          {
-            "border-white": borderless,
-            "!w-full": fullWidth,
-            "bg-accent-blurple text-white": !light,
-            "bg-white text-accent-blurple hover:text-white": light,
-          }
+          "b3 block rounded border-2 p-4 !font-600 transition-all hover:border-dark-blurple hover:bg-dark-blurple",
+          borderless ? "border-white" : "border-accent-blurple",
+          fullWidth ? "w-full" : "w-max",
+          light
+            ? "bg-white text-accent-blurple hover:text-white"
+            : "bg-accent-blurple text-white"
         )}
         target={isExternalURL(href) && "_blank"}
         rel={isExternalURL(href) && "noopener noreferrer"}
