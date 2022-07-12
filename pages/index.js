4efbea5b2bf70040c47f07d9b3a243673def7aab
Mika Busante <mika@mikabusante.com>
2022-07-12 13:55:18 -0400

    add features section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { FormattedMessage } from "react-intl"
+import Image from "next/image"
+import classnames from "classnames"
 
 import loadIntlMessages from "../utils/loadIntlMessages"
 import LinkButton from "../components/LinkButton"
@@ -6,11 +8,16 @@ import TestimonialCard from "../components/TestimonialCard"
 
 import testimonialsData from "../data/testimonials.json"
 
+import illoTimeline from "../public/illustrations/features_timeline.png"
+import illoAudience from "../public/illustrations/features_audience.png"
+import illoModeration from "../public/illustrations/features_moderation.png"
+
 function Home({ testimonials }) {
   return (
     <>
       <HomeHero />
       <Testimonials testimonials={testimonialsData.slice(0, 3)} />
+      <Features />
     </>
   )
 }
@@ -56,20 +63,135 @@ const HomeHero = () => {
 const Testimonials = ({ testimonials }) => {
   return (
     <section className="full-width-bg bg-gray-5 pt-20 pb-28">
-      <h2 className="h3 pb-16 text-center">
-        <FormattedMessage
-          id="home.testimonials.title"
-          defaultMessage="What our users are saying"
-        />
-      </h2>
-      <div className="m-auto grid max-w-site gap-gutter px-6 md:grid-cols-3 lg:px-16">
-        {/* TODO(mika): these cards should be rendered in a carousel*/}
-        {testimonials.map((testimonial) => {
-          return (
-            <TestimonialCard key={testimonial.name} testimonial={testimonial} />
-          )
-        })}
+      <div className="full-width-bg__inner">
+        <h2 className="h3 pb-16 text-center">
+          <FormattedMessage
+            id="home.testimonials.title"
+            defaultMessage="What our users are saying"
+          />
+        </h2>
+        <div className=" grid gap-gutter  md:grid-cols-3 ">
+          {/* TODO(mika): these cards should be rendered in a carousel*/}
+          {testimonials.map((testimonial) => {
+            return (
+              <TestimonialCard
+                key={testimonial.name}
+                testimonial={testimonial}
+              />
+            )
+          })}
+        </div>
       </div>
+    </section>
+  )
+}
+
+const Features = () => {
+  return (
+    <section>
+      {[
+        {
+          title: (
+            <FormattedMessage
+              id="home.features.timeline.title"
+              defaultMessage="Stay in control of your own timeline"
+            />
+          ),
+          body: (
+            <FormattedMessage
+              id="home.features.timeline.body"
+              defaultMessage="Decentralized social media is created and maintained by its users, making you the owner of your feed. You can be as public or as private as you like and curate your own experience."
+            />
+          ),
+          button: (
+            <LinkButton href="/">
+              <FormattedMessage
+                id="home.features.button.learn_more"
+                defaultMessage="Learn more"
+              />
+            </LinkButton>
+          ),
+          image: illoTimeline,
+        },
+        {
+          title: (
+            <FormattedMessage
+              id="home.features.audience.title"
+              defaultMessage="Build your audience"
+            />
+          ),
+          body: (
+            <FormattedMessage
+              id="home.features.audience.body"
+              defaultMessage="Joining a server on Mastodon provides you with the power to communicate with any server across the globe. Meet like-minded people and make your corner of the internet a little more like you. Need more from Mastodon? Host your own server."
+            />
+          ),
+          button: (
+            <LinkButton href="/">
+              <FormattedMessage
+                id="home.features.button.learn_more"
+                defaultMessage="Learn more"
+              />
+            </LinkButton>
+          ),
+          image: illoAudience,
+        },
+        {
+          title: (
+            <FormattedMessage
+              id="home.features.moderation.title"
+              defaultMessage="Moderating the way it should be"
+            />
+          ),
+          body: (
+            <FormattedMessage
+              id="home.features.moderation.body"
+              defaultMessage="Mastodon puts decision making back in the hands of the people. Each server creates their own rules and regulations, which are enforced locally and not top-down like corporate social media. Self-hosting grants even more control by letting you decide the rules."
+            />
+          ),
+          button: (
+            <LinkButton href="/">
+              <FormattedMessage
+                id="home.features.button.find_a_server"
+                defaultMessage="Find a server"
+              />
+            </LinkButton>
+          ),
+          image: illoModeration,
+        },
+      ].map((block, i) => {
+        const isOdd = i % 2 != 0
+        return (
+          <div className={classnames(isOdd && "full-width-bg bg-gray-5")}>
+            <div
+              className={classnames(
+                "pt-14 pb-[4.5rem] lg:grid lg:grid-cols-12 lg:gap-gutter",
+                isOdd && "full-width-bg__inner"
+              )}
+            >
+              <div
+                className={classnames(
+                  "row-start-1",
+                  isOdd ? "lg:col-[8_/_span_5]" : "lg:col-[2_/_span_5]"
+                )}
+              >
+                <Image src={block.image} alt="" />
+              </div>
+
+              <div
+                className={classnames(
+                  "row-start-1",
+                  isOdd ? "lg:col-[2_/_span_5]" : "lg:col-[8_/_span_5]"
+                )}
+              >
+                <h2 className="h4 lg:h2 mb-2">{block.title}</h2>
+                <p className="b1 mb-8">{block.body}</p>
+                {block.button}
+              </div>
+            </div>
+          </div>
+        )
+      })}
     </section>
   )
 }
