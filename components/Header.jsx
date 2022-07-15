94d5fe8b39618e421db38a2f09ecfa42a42b1cd3
Daniel Gamage <hello@danielgamage.com>
2022-07-14 22:33:37 -0400

    moved main menu's keyboard, aria logic to a hook to group the logic and leave the jsx primarily to styling, templating concerns
    it's longer in terms of lines, but i think the gains in readability and separation (or rather grouping) of concerns are worthwhile

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,21 +3,149 @@ import { FormattedMessage } from "react-intl"
 
 import mastodonLogo from "../public/logos/logo-full-purple.svg"
 import Image from "next/image"
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react"
 import classNames from "classnames"
 import { locales } from "../data/locales"
 import MenuToggle from "./MenuToggle"
 import SVG from "react-inlinesvg"
-import { useEffect } from "react"
+
+const useMenu = ({ navigationItems }) => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [primaryMenuItemIndex, setPrimaryMenuItemIndex] = useState(0)
+  const [secondaryMenuItemIndex, setSecondaryMenuItemIndex] = useState(null)
+  const rootElement = useRef(null)
+
+  // Navigation
+  const navigateHorizontally = (direction) => {
+    setPrimaryMenuItemIndex(
+      (primaryMenuItemIndex + direction + navigationItems.length) %
+        navigationItems.length
+    )
+    setSecondaryMenuItemIndex(null)
+  }
+  const navigateVertically = (direction) => {
+    const secondaryItems = navigationItems[primaryMenuItemIndex].options
+    if (secondaryItems) {
+      if (secondaryMenuItemIndex === null) {
+        setSecondaryMenuItemIndex(
+          direction === 1 ? 0 : secondaryItems.length - 1
+        )
+      } else {
+        setSecondaryMenuItemIndex(
+          ((secondaryMenuItemIndex || 0) + direction + secondaryItems.length) %
+            secondaryItems.length
+        )
+      }
+    }
+  }
+
+  // Ensuring document.activeElement follows the menu's roving tabindex
+  useEffect(
+    function updateFocus() {
+      rootElement.current
+        .querySelector(`[tabindex="0"]`)
+        .focus({ preventScroll: true })
+    },
+    [primaryMenuItemIndex, secondaryMenuItemIndex]
+  )
+
+  // Element attributes / listeners
+  const bindToggle = () => ({
+    open: mobileMenuOpen,
+    onClick: () => setMobileMenuOpen(!mobileMenuOpen),
+  })
+  const bindPrimaryMenu = () => {
+    return {
+      role: "menubar",
+      ref: rootElement,
+      onKeyDown: (e) => {
+        if (e.key === "Escape") {
+          setMobileMenuOpen(false)
+        }
+        if (
+          ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"].includes(e.key)
+        ) {
+          e.preventDefault()
+          switch (e.key) {
+            case "ArrowLeft":
+              navigateHorizontally(-1)
+              break
+            case "ArrowRight":
+              navigateHorizontally(+1)
+              break
+            case "ArrowUp":
+              navigateVertically(-1)
+              break
+            case "ArrowDown":
+              navigateVertically(+1)
+              break
+          }
+        }
+      },
+    }
+  }
+  const bindPrimaryMenuItem = (
+    itemIndex,
+    { hasPopup } = { hasPopup: false }
+  ) => {
+    return {
+      role: "menuitem",
+      "aria-haspopup": hasPopup,
+      "aria-expanded":
+        hasPopup &&
+        (primaryMenuItemIndex !== itemIndex || secondaryMenuItemIndex === null),
+      tabIndex:
+        primaryMenuItemIndex === itemIndex && secondaryMenuItemIndex === null
+          ? 0
+          : -1,
+      onKeyDown: (e) => {
+        if (e.key === "Enter" || e.key === " ") {
+          e.preventDefault()
+          setSecondaryMenuItemIndex(0)
+        }
+      },
+      onClick: () => {
+        if (itemIndex === primaryMenuItemIndex) {
+          setSecondaryMenuItemIndex(secondaryMenuItemIndex === null ? 0 : null)
+        } else {
+          setPrimaryMenuItemIndex(itemIndex)
+          setSecondaryMenuItemIndex(0)
+        }
+      },
+    }
+  }
+  const bindSecondaryMenu = () => ({ role: "menu" })
+  const bindSecondaryMenuItem = (parentIndex, itemIndex) => ({
+    tabIndex:
+      parentIndex === primaryMenuItemIndex &&
+      itemIndex === secondaryMenuItemIndex
+        ? 0
+        : -1,
+    onKeyDown: (e) => {
+      if (e.key === "Escape") {
+        setSecondaryMenuItemIndex(null)
+      }
+    },
+    role: "menuitem",
+  })
+
+  return {
+    mobileMenuOpen,
+    primaryMenuItemIndex,
+    secondaryMenuItemIndex,
+    bindToggle,
+    bindPrimaryMenu,
+    bindPrimaryMenuItem,
+    bindSecondaryMenu,
+    bindSecondaryMenuItem,
+  }
+}
 
 /**
  * @see https://www.w3.org/WAI/ARIA/apg/example-index/disclosure/disclosure-navigation-hybrid.html
  */
 const Header = () => {
   const [pageScrolled, setPageScrolled] = useState(false)
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-  const [primaryMenuItemIndex, setPrimaryMenuItemIndex] = useState(0)
-  const [secondaryMenuItemIndex, setSecondaryMenuItemIndex] = useState(null)
 
   const navigationItems = [
     {
@@ -64,6 +192,17 @@ const Header = () => {
     },
   ]
 
+  const {
+    mobileMenuOpen,
+    primaryMenuItemIndex,
+    secondaryMenuItemIndex,
+    bindToggle,
+    bindPrimaryMenu,
+    bindPrimaryMenuItem,
+    bindSecondaryMenu,
+    bindSecondaryMenuItem,
+  } = useMenu({ navigationItems })
+
   const checkPageScroll = (e) => {
     if (window.scrollY > 0) {
       setPageScrolled(true)
@@ -76,48 +215,6 @@ const Header = () => {
     checkPageScroll()
   }, [])
 
-  useEffect(
-    function updateFocus() {
-      const currentItem = document
-        .querySelectorAll(`[role="menubar"] > li`)
-        .item(primaryMenuItemIndex)
-      let targetItem = currentItem
-      if (
-        secondaryMenuItemIndex !== null &&
-        currentItem.getAttribute("aria-haspopup") === "true"
-      ) {
-        targetItem = currentItem
-          .querySelectorAll(`li`)
-          .item(secondaryMenuItemIndex)
-      }
-      targetItem.querySelector("a, button").focus({ preventScroll: true })
-    },
-    [primaryMenuItemIndex, secondaryMenuItemIndex]
-  )
-
-  const navigateHorizontally = (direction) => {
-    setPrimaryMenuItemIndex(
-      (primaryMenuItemIndex + direction + navigationItems.length) %
-        navigationItems.length
-    )
-    setSecondaryMenuItemIndex(null)
-  }
-  const navigateVertically = (direction) => {
-    const secondaryItems = navigationItems[primaryMenuItemIndex].options
-    if (secondaryItems) {
-      if (secondaryMenuItemIndex === null) {
-        setSecondaryMenuItemIndex(
-          direction === 1 ? 0 : secondaryItems.length - 1
-        )
-      } else {
-        setSecondaryMenuItemIndex(
-          ((secondaryMenuItemIndex || 0) + direction + secondaryItems.length) %
-            secondaryItems.length
-        )
-      }
-    }
-  }
-
   return (
     <div className="sticky top-0 z-10 mx-auto flex items-center justify-between py-4 text-white">
       <div
@@ -127,19 +224,17 @@ const Header = () => {
         )}
       />
       <div>
-        <Link className="brand" href="/">
+        <Link href="/">
           <a className="flex max-w-[11.375rem] md:max-w-[12.625rem]">
-            <Image className="link-logo" src={mastodonLogo} alt="Mastodon" />
+            <Image src={mastodonLogo} alt="Mastodon" />
           </a>
         </Link>
       </div>
 
       <nav>
-        <MenuToggle
-          open={mobileMenuOpen}
-          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-        />
+        <MenuToggle {...bindToggle()} />
         <ul
+          {...bindPrimaryMenu()}
           className={classNames(
             "absolute w-screen gap-4 md:relative md:w-auto md:gap-10 md:p-4",
             {
@@ -147,107 +242,39 @@ const Header = () => {
               flex: mobileMenuOpen,
             }
           )}
-          role="menubar"
-          onKeyDown={(e) => {
-            if (e.key === "Escape") {
-              setMobileMenuOpen(false)
-            }
-            if (
-              ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"].includes(
-                e.key
-              )
-            ) {
-              e.preventDefault()
-              switch (e.key) {
-                case "ArrowLeft":
-                  navigateHorizontally(-1)
-                  break
-                case "ArrowRight":
-                  navigateHorizontally(+1)
-                  break
-                case "ArrowUp":
-                  navigateVertically(-1)
-                  break
-                case "ArrowDown":
-                  navigateVertically(+1)
-                  break
-              }
-            }
-          }}
         >
           {navigationItems.map((item, itemIndex) => (
-            <li
-              className="relative"
-              key={item.key || item.value}
-              onBlur={() => {
-                console.log("blurred")
-              }}
-            >
+            <li className="relative" key={item.key || item.value}>
               {Boolean(item.options) ? (
                 <>
                   <button
-                    role="menuitem"
-                    aria-haspopup
-                    aria-expanded={
-                      primaryMenuItemIndex !== itemIndex ||
-                      secondaryMenuItemIndex === null
-                    }
+                    {...bindPrimaryMenuItem(itemIndex, { hasPopup: true })}
                     className="flex items-center gap-[0.125rem] whitespace-nowrap focus:outline-2"
-                    tabIndex={
-                      primaryMenuItemIndex === itemIndex &&
-                      secondaryMenuItemIndex === null
-                        ? 0
-                        : -1
-                    }
-                    onKeyDown={(e) => {
-                      if (e.key === "Enter" || e.key === " ") {
-                        e.preventDefault()
-                        setSecondaryMenuItemIndex(0)
-                      }
-                    }}
-                    onClick={() => {
-                      if (itemIndex === primaryMenuItemIndex) {
-                        setSecondaryMenuItemIndex(
-                          secondaryMenuItemIndex === null ? 0 : null
-                        )
-                      } else {
-                        setPrimaryMenuItemIndex(itemIndex)
-                        setSecondaryMenuItemIndex(0)
-                      }
-                    }}
                   >
                     {item.label}
                     <SVG
                       src={"/ui/disclosure-arrow.svg"}
                       className={classNames({
-                        "rotate-180": mobileMenuOpen,
+                        "rotate-180":
+                          primaryMenuItemIndex === itemIndex &&
+                          secondaryMenuItemIndex !== null,
                       })}
                     />
                   </button>
 
                   <ul
+                    {...bindSecondaryMenu()}
                     className={classNames(
                       "absolute top-[100%] -right-4 flex flex-col rounded bg-eggplant p-4 md:shadow",
                       (primaryMenuItemIndex !== itemIndex ||
                         secondaryMenuItemIndex === null) &&
                         "md:sr-only"
                     )}
-                    role="menu"
                   >
                     {item.options.map((option, optionIndex) => (
                       <li key={option.value}>
                         <Link href={option.value}>
-                          <a
-                            tabIndex={
-                              secondaryMenuItemIndex === optionIndex ? 0 : -1
-                            }
-                            onKeyDown={(e) => {
-                              if (e.key === "Escape") {
-                                setSecondaryMenuItemIndex(null)
-                              }
-                            }}
-                            role="menuitem"
-                          >
+                          <a {...bindSecondaryMenuItem(itemIndex, optionIndex)}>
                             {option.label}
                           </a>
                         </Link>
@@ -258,9 +285,8 @@ const Header = () => {
               ) : (
                 <Link href={item.value}>
                   <a
-                    className={"whitespace-nowrap"}
-                    role="menuitem"
-                    tabIndex={primaryMenuItemIndex === itemIndex ? 0 : -1}
+                    className="whitespace-nowrap"
+                    {...bindPrimaryMenuItem(itemIndex)}
                   >
                     {item.label}
                   </a>
