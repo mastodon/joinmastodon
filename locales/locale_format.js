dc8d34fb875aa960f4c1b19453637b2648b98def
Mika Busante <mika@mikabusante.com>
2022-07-18 16:23:27 -0400

    add comment about replacing default formatter

--- locales/locale_format.js
+++ locales/locale_format.js
@@ -1,3 +1,9 @@
+// Formatter file for extracting messages. Replaces default formatter. see https://formatjs.io/docs/tooling/cli#--format-path
+// Message files will be in this format:
+// {
+//   [key]: string
+// }
+
 const format = (msgs) => {
   const results = {}
   for (const key in msgs) {
