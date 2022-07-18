dedc94ca0ec4269b87b66feebdfabffc9c801144
Daniel Gamage <hello@danielgamage.com>
2022-07-18 10:55:42 -0400

    importing locale list from /data to nextconfig

--- next.config.js
+++ next.config.js
@@ -1,35 +1,10 @@
+import { locales } from "../data/locales"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: [
-      "ar",
-      "ca",
-      "cs",
-      "cy",
-      "de",
-      "en",
-      "es",
-      "eu",
-      "fi",
-      "fr",
-      "he",
-      "it",
-      "ja",
-      "ko",
-      "nl-NL",
-      "no",
-      "pl",
-      "pt-BR",
-      "ru",
-      "sk",
-      "sq",
-      "sv",
-      "th",
-      "tr",
-      "uk",
-      "zh",
-    ],
+    locales,
     defaultLocale: "en",
   },
 }
