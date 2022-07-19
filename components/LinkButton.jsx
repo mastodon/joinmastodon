7bd2691f97d908f533e0f32a65053534f04361a6
Mika Busante <mika@mikabusante.com>
2022-07-19 15:13:06 -0400

    define large, medium, and small variants on LinkButton

--- components/LinkButton.jsx
+++ components/LinkButton.jsx
@@ -8,6 +8,8 @@ const LinkButton = ({
   href,
   light,
   large,
+  medium = true,
+  small,
 }) => {
   let linkAttrs = {}
 
@@ -21,13 +23,15 @@ const LinkButton = ({
     <Link href={href}>
       <a
         className={classnames(
-          "block rounded border-2 p-4 text-center !font-600 transition-all hover:border-dark-blurple hover:bg-dark-blurple",
+          "flex items-center justify-center rounded border-2 p-4 text-center !font-600 transition-all hover:border-dark-blurple hover:bg-dark-blurple",
           borderless ? "border-white" : "border-accent-blurple",
           fullWidth ? "w-full" : "w-max",
           light
             ? "bg-white text-accent-blurple hover:text-white"
             : "bg-accent-blurple text-white",
-          large ? "b1" : "b3"
+          large && "b1 h-16",
+          medium && "b3 h-12",
+          small && "b3 h-10"
         )}
         {...linkAttrs}
       >
--- components/ServerCard.jsx
+++ components/ServerCard.jsx
@@ -14,7 +14,7 @@ let server = {
     "https://proxy.joinmastodon.org/d7d02f9615184131475feeb95ab8cd01e6575448/68747470733a2f2f66696c65732e6d6173746f646f6e2e736f6369616c2f736974655f75706c6f6164732f66696c65732f3030302f3030302f3030312f6f726967696e616c2f766c63736e61702d323031382d30382d32372d31366834336d3131733132372e706e67",
   total_users: 725761,
   last_week_users: 43440,
-  approval_required: false,
+  approval_required: true,
   language: "en",
   category: "general",
 }
@@ -36,9 +36,18 @@ const ServerCard = () => {
         <p className="b3 mb-4 capitalize text-gray-1">{server.category}</p>
         <p className="b3 mb-5">{server.description}</p>
 
-        <LinkButton href={`https://${server.domain}`} fullWidth>
-          <FormattedMessage id="join_server" defaultMessage="Join server" />
-        </LinkButton>
+        {server.approval_required ? (
+          <LinkButton href={`https://${server.domain}`} fullWidth light small>
+            <FormattedMessage
+              id="request_access"
+              defaultMessage="Request access"
+            />
+          </LinkButton>
+        ) : (
+          <LinkButton href={`https://${server.domain}`} fullWidth small>
+            <FormattedMessage id="join_server" defaultMessage="Join server" />
+          </LinkButton>
+        )}
       </div>
     </div>
   )
