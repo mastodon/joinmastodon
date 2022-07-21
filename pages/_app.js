29c9d0bb84c9d0cd3faf8d81f5c673471aab775a
Mika Busante <mika@mikabusante.com>
2022-07-21 12:35:09 -0400

    add react-query dev tools

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import { useRouter } from "next/router"
 import { IntlProvider } from "react-intl"
 import { useEffect } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
 import Layout from "../components/Layout"
 import { getDirForLocale } from "../utils/locales"
 import "../styles/globals.scss"
@@ -25,6 +27,7 @@ function MyApp({ Component, pageProps }) {
         <Layout>
           <Component {...pageProps} />
         </Layout>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </IntlProvider>
   )
