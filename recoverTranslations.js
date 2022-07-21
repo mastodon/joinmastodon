28f4832bc99fe8761ec579c36874f6b14f895964
Mika Busante <mika@mikabusante.com>
2022-07-21 16:23:11 -0400

    edit variable name

--- recoverTranslations.js
+++ recoverTranslations.js
@@ -1,12 +1,12 @@
 const fs = require("fs")
 
-let archivedMessages = fs.readdirSync("./z_archive/locales")
+let oldMessages = fs.readdirSync("./z_archive/locales")
 let oldDefaultMessages = JSON.parse(
   fs.readFileSync("./z_archive/locales/old_default_messages.json", "utf-8")
 )
 let newSource = JSON.parse(fs.readFileSync("./locales/en.json", "utf-8"))
 
-archivedMessages.forEach((localeFile) => {
+oldMessages.forEach((localeFile) => {
   if (localeFile === "en.json" || localeFile === "old_default_messages.json")
     return // don't overwrite the source file or old default messages
 
