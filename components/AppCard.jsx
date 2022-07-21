432f9cf7e09e29d62a53efc3fc960f5d7d3ef6cb
Daniel Gamage <hello@danielgamage.com>
2022-07-21 10:52:21 -0400

    prevent app logo from shrinking with longer text

--- components/AppCard.jsx
+++ components/AppCard.jsx
@@ -15,15 +15,21 @@ import { FormattedMessage } from "react-intl"
 export const AppCard = ({ name, icon, url, paid }) => {
   return (
     <a href={url}>
-    <div className="flex items-center gap-4 justify-start p-2 md:p-4 bg-white shadow rounded">
-      <div className="w-[3.5rem] h-[3.5rem]">
-        <Image src={icon} />
+      <div className="flex items-center justify-start gap-4 rounded bg-white p-2 shadow md:p-4">
+        <div className="h-[3.5rem] w-[3.5rem] flex-shrink-0">
+          <Image src={icon} />
+        </div>
+        <div className="flex flex-col">
+          <span className="b4 text-gray-1">
+            {paid ? (
+              <FormattedMessage id="apps.paid" defaultMessage="Paid" />
+            ) : (
+              <FormattedMessage id="apps.free" defaultMessage="Free" />
+            )}
+          </span>
+          <h3 className="b1 font-800 !leading-[1]">{name}</h3>
+        </div>
       </div>
-      <div className="flex flex-col">
-        <span className="b4 text-gray-1">{paid ? <FormattedMessage id='apps.paid' defaultMessage='Paid' /> : <FormattedMessage id='apps.free' defaultMessage='Free' />}</span>
-        <h3 className="b1 font-800">{name}</h3>
-      </div>
-    </div>
     </a>
   )
 }
