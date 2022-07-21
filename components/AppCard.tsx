9b19a16f47edfdb8295597a0d1c2848fcdae891d
Daniel Gamage <hello@danielgamage.com>
2022-07-21 15:45:39 -0400

    appcard typescript conversion

--- components/AppCard.tsx
+++ components/AppCard.tsx
@@ -3,31 +3,35 @@ import Image from "next/image"
 import SVG from "react-inlinesvg"
 import { FormattedMessage } from "react-intl"
 
+export type AppCardProps = {
+  name: React.ReactNode
+  icon: string
+  url: URL
+  paid: boolean
+}
+
 /**
  * IconCard component.
  * Layout (width, height, positioning) can be set from the parent.
- * @param {{
- *  title: React.ReactNode,
- *  icon: string,
- *  copy: React.ReactNode
- * }} props 
  */
 export const AppCard = ({ name, icon, url, paid }) => {
   return (
-    <a href={url}>
-      <div className="flex items-center justify-start gap-4 rounded bg-white p-2 shadow md:p-4">
-        <div className="h-[3.5rem] w-[3.5rem] flex-shrink-0">
-          <Image src={icon} />
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <div className="flex items-stretch justify-start gap-4 rounded bg-white p-2 shadow md:p-4">
+        <div className="h-[3.5rem] w-[3.5rem] flex-shrink-0 overflow-hidden rounded-sm">
+          <Image src={icon} alt={`Logo for ${name}`} />
         </div>
         <div className="flex flex-col">
-          <span className="b4 text-gray-1">
+          <span className="b4 block text-gray-1">
             {paid ? (
               <FormattedMessage id="apps.paid" defaultMessage="Paid" />
             ) : (
               <FormattedMessage id="apps.free" defaultMessage="Free" />
             )}
           </span>
-          <h3 className="b1 font-800 !leading-[1]">{name}</h3>
+          <h3 className="b1 flex flex-auto items-center font-800 !leading-[1] [unicode-bidi:normal]">
+            {name}
+          </h3>
         </div>
       </div>
     </a>
