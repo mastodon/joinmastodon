75212fa9ef8579d3839798a144646f40f5ab5083
Mika Busante <mika@mikabusante.com>
2022-07-21 16:19:42 -0400

    update `recoverTranslations` to check default messages

--- recoverTranslations.js
+++ recoverTranslations.js
@@ -1,10 +1,14 @@
 const fs = require("fs")
 
 let archivedMessages = fs.readdirSync("./z_archive/locales")
+let oldDefaultMessages = JSON.parse(
+  fs.readFileSync("./z_archive/locales/old_default_messages.json", "utf-8")
+)
 let newSource = JSON.parse(fs.readFileSync("./locales/en.json", "utf-8"))
 
 archivedMessages.forEach((localeFile) => {
-  if (localeFile === "en.json") return // don't overwrite the source file
+  if (localeFile === "en.json" || localeFile === "old_default_messages.json")
+    return // don't overwrite the source file or old default messages
 
   let oldMessages = JSON.parse(
     fs.readFileSync(`./z_archive/locales/${localeFile}`, "utf-8")
@@ -12,10 +16,15 @@ archivedMessages.forEach((localeFile) => {
 
   let result = {}
   for (let newSourceKey in newSource) {
-    if (oldMessages[newSourceKey]) {
-      result[newSourceKey] = oldMessages[newSourceKey]
+    for (let oldDefaultMessageKey in oldDefaultMessages) {
+      if (
+        oldDefaultMessages[oldDefaultMessageKey].defaultMessage ===
+        newSource[newSourceKey]
+      ) {
+        result[newSourceKey] = oldMessages[oldDefaultMessageKey]
+      }
     }
-
-    fs.writeFileSync(`./locales/${localeFile}`, JSON.stringify(result))
   }
+
+  fs.writeFileSync(`./locales/${localeFile}`, JSON.stringify(result))
 })
