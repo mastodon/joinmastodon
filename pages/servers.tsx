8fc04fffd881923de4eac7c11dd29043d1ecc7a3
Mika Busante <mika@mikabusante.com>
2022-07-25 16:17:28 -0400

    rm unused code, comment out server stuff for now

--- pages/servers.tsx
+++ pages/servers.tsx
@@ -5,36 +5,32 @@ import ServerCard from "../components/ServerCard"
 import { categoriesMessages } from "../data/categories"
 import type Server from "../types/server"
 
-const Servers = ({ filterList, servers }) => {
-  // const [filters, setFilters] = useState({
-  //   topic: filterData.topic.map((item) => ({ ...item, checked: false })),
-  //   // language: filters.language.map((item) => ({ ...item, checked: false })),
-  //   // server_size: filters.server_size.map((item) => ({
-  //   //   ...item,
-  //   //   checked: false,
-  //   // })),
-  // })
+const Servers = ({ filterList }) => {
+  const [filters, setFilters] = useState({ language: "", topic: "" })
 
-  // for (let group in filters) {
-  //   filters[group].forEach((item) => {
-  //     if (item.checked) {
-  //       console.log(
-  //         "filtered",
-  //         servers.filter((server) => server.categories.includes(item.label))
-  //       )
-  //     }
-  //   })
+  const servers = useQuery(
+    [`servers-${filters.language}-${filters.topic}`],
+    async function (): Promise<Server[]> {
+      const res = await fetch(
+        `https://api.joinmastodon.org/servers?language=${filters.language}&category=${filters.topic}`
+      )
+      return await res.json()
+    },
+    { cacheTime: 30 * 60 * 1000 }
+  ) // 30 minutes)
 
-  // }
-
-  console.log(filterList)
+  console.log(servers.data)
 
   return (
     <div className="py-40">
       <h1>Servers page placeholder</h1>
 
       <div className="grid grid-cols-4 gap-gutter lg:grid-cols-12">
-        <ServerFilters filterList={filterList} />
+        <ServerFilters
+          filterList={filterList}
+          filters={filters}
+          setFilters={setFilters}
+        />
         <ServerList servers={servers} />
       </div>
     </div>
@@ -42,22 +38,13 @@ const Servers = ({ filterList, servers }) => {
 }
 
 const ServerList = ({ servers }) => {
-  // const servers = useQuery(
-  //   ["servers"],
-  //   async function (): Promise<Server[]> {
-  //     const res = await fetch("https://api.joinmastodon.org/servers")
-  //     return await res.json()
-  //   },
-  //   { cacheTime: 30 * 60 * 1000 } // 30 minutes
-  // )
+  if (servers.isLoading) {
+    return <p>Loading...</p>
+  }
 
-  // if (servers.isLoading) {
-  //   return <p>Loading...</p>
-  // }
-
-  // if (servers.isError) {
-  //   return <p>Oops, something went wrong.</p>
-  // }
+  if (servers.isError) {
+    return <p>Oops, something went wrong.</p>
+  }
 
   return (
     <div className="col-span-4 lg:col-start-4 lg:col-end-13 ">
@@ -65,7 +52,7 @@ const ServerList = ({ servers }) => {
         <FormattedMessage id="servers.browse_all" defaultMessage="Browse all" />
       </h3>
       <div className="grid gap-gutter md:grid-cols-2  xl:grid-cols-3">
-        {servers?.map((server) => (
+        {servers.data.map((server) => (
           <ServerCard key={server.domain} server={server} />
         ))}
       </div>
@@ -73,7 +60,15 @@ const ServerList = ({ servers }) => {
   )
 }
 
-const ServerFilters = ({ filterList }: { filterList: any }) => {
+const ServerFilters = ({
+  filters,
+  setFilters,
+  filterList,
+}: {
+  filters: any
+  setFilters: any
+  filterList: any
+}) => {
   const intl = useIntl()
   const filterGroupMessages = defineMessages({
     topic: { id: "server.filter_by.topic", defaultMessage: "Topic" },
@@ -87,12 +82,6 @@ const ServerFilters = ({ filterList }: { filterList: any }) => {
     },
   })
 
-  console.log(filterList)
-
-  // const isChecked = (item) => {
-  //   return filters.some((filter) => filter.label === item.label)
-  // }
-
   return (
     <div className="col-span-3">
       {Object.keys(filterList).map((group, i) => {
@@ -104,7 +93,19 @@ const ServerFilters = ({ filterList }: { filterList: any }) => {
             <ul>
               {filterList[group].map((item, i) => {
                 return (
-                  <li className="b2 flex gap-1" key={i}>
+                  <li
+                    className="b2 flex cursor-pointer gap-1"
+                    key={i}
+                    onClick={() => {
+                      if (group === "topic") {
+                        setFilters({ ...filters, topic: item.category })
+                      }
+
+                      if (group === "language") {
+                        setFilters({ ...filters, language: item.locale })
+                      }
+                    }}
+                  >
                     {group === "topic"
                       ? intl.formatMessage(categoriesMessages[item.category])
                       : item.language || item.server_size}
@@ -117,91 +118,6 @@ const ServerFilters = ({ filterList }: { filterList: any }) => {
           </div>
         )
       })}
-      {/* {Object.keys(filters).map((group, i) => {
-        return (
-          <div key={i}>
-            <h3>title here</h3>
-
-            <ul>
-              {/* {filters[group].map((item, i) => {
-                return (
-                  <li
-                    className="cursor-pointer"
-                    role="checkbox"
-                    key={i}
-                    aria-checked={item.checked}
-                    onClick={() => {
-                      let item = filters[group][i]
-                      item.checked = !item.checked
-                      filters[group].splice(i, 1, item)
-                      setFilters({ ...filters })
-                    }}
-                  >
-                    {item.label}
-                  </li>
-                )
-              })} */}
-      {/* </ul>
-          </div>
-        )
-      })} */}
-
-      {/* {Object.keys(filters).map((group) => {
-        return (
-          <div className="mb-8" key={group}>
-            <h3 className="h5 mb-2" id={`${group}-group-label`}>
-              {intl.formatMessage(filterGroupMessages[group])}
-            </h3>
-
-            <ul
-              className="space-y-2"
-              role="group"
-              aria-labelledby={`${group}-group-label`}
-            >
-              {filters[group]?.map((item, i) => {
-                if (item.label || item.language || item.server_size) {
-                  return (
-                    <li key={i}>
-                      <div
-                        className="flex w-max cursor-pointer gap-1"
-                        role="checkbox"
-                        tabIndex={0}
-                        aria-checked={
-                          filterState[group].find(
-                            (filter) => filter.label === item.label
-                          )?.checked
-                        }
-                        onClick={() => {
-                          const updatedFilterState = filterState[group].map(
-                            (filter) => {
-                              if (filter.label === item.label) {
-                                filter.checked = !filter.checked
-                              }
-                              return filter
-                            }
-                          )
-
-                          setFilterState({
-                            ...filterState,
-                            [group]: updatedFilterState,
-                          })
-                        }}
-                      >
-                        <span className="pl-2">
-                          {group === "topic"
-                            ? intl.formatMessage(categoriesMessages[item.label])
-                            : item.language || item.server_size}
-                        </span>
-                        <span className="text-gray-2">({item.count})</span>
-                      </div>
-                    </li>
-                  )
-                }
-              })}
-            </ul>
-          </div>
-        )
-      })} */}
     </div>
   )
 }
@@ -213,42 +129,42 @@ export async function getServerSideProps() {
   const langaugeRes = await fetch("https://api.joinmastodon.org/languages")
   const language = await langaugeRes.json()
 
-  const serversRes = await fetch("https://api.joinmastodon.org/servers")
-  const servers = await serversRes.json()
+  // const serversRes = await fetch("https://api.joinmastodon.org/servers")
+  // const servers = await serversRes.json()
 
-  // matching data format of /categories and /languages
-  let serverCount = [
-    {
-      server_size: "1 - 1,000",
-      servers_count: 0,
-    },
-    {
-      server_size: "1,000 - 5,000",
-      servers_count: 0,
-    },
-    {
-      server_size: "5,000+",
-      servers_count: 0,
-    },
-  ]
+  // // matching data format of /categories and /languages
+  // let serverCount = [
+  //   {
+  //     server_size: "1 - 1,000",
+  //     servers_count: 0,
+  //   },
+  //   {
+  //     server_size: "1,000 - 5,000",
+  //     servers_count: 0,
+  //   },
+  //   {
+  //     server_size: "5,000+",
+  //     servers_count: 0,
+  //   },
+  // ]
 
-  servers.forEach((server) => {
-    if (server.total_users < 1000) {
-      serverCount[0].servers_count++
-    } else if (server.total_users > 1000 && server.total_users <= 5000) {
-      serverCount[1].servers_count++
-    } else {
-      serverCount[2].servers_count++
-    }
-  })
+  // servers.forEach((server) => {
+  //   if (server.total_users < 1000) {
+  //     serverCount[0].servers_count++
+  //   } else if (server.total_users > 1000 && server.total_users <= 5000) {
+  //     serverCount[1].servers_count++
+  //   } else {
+  //     serverCount[2].servers_count++
+  //   }
+  // })
 
   return {
     props: {
-      servers,
+      // servers,
       filterList: {
         topic,
         language,
-        server_size: serverCount,
+        // server_size: serverCount,
       },
     },
   }
