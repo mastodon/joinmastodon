d87212492b6b09f1eba1da6d910d57ffe7848c57
Mika Busante <mika@mikabusante.com>
2022-07-26 11:03:00 -0400

    add `SponsorCard` component

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -1,6 +1,8 @@
+import classnames from "classnames"
 import Image from "next/image"
 import { FormattedMessage } from "react-intl"
 import SponsorGroup from "../components/SponsorGroup"
+import sponsors from "../data/sponsors"
 import sponsorData from "../data/sponsors"
 
 function Sponsors() {
@@ -14,31 +16,36 @@ function Sponsors() {
         />
       </section>
 
-      <section className="silver-sponsors">
-        <h2 className="h5">
+      <section className="silver-sponsors mb-32">
+        <h2 className="h5 mb-8">
           <FormattedMessage id="sponsors" defaultMessage="Sponsors" />
         </h2>
         <div className="grid gap-y-8 lg:grid-cols-4 lg:gap-x-5">
           {sponsorData.silver.map((sponsor) => {
+            return <SponsorCard sponsor={sponsor} tier="silver" />
+          })}
+        </div>
+      </section>
+      <section className="general-sponsors mb-96">
+        <h2 className="h5 mb-8">
+          <FormattedMessage
+            id="sponsors.additional_thanks_to"
+            defaultMessage="Additional thanks to"
+          />
+        </h2>
+        <div className="grid gap-y-8 md:grid-cols-4 lg:grid-cols-6 lg:gap-x-6">
+          {sponsors.generalHighlighted.map((sponsor) => {
             return (
-              <div className="grid grid-cols-[auto_1fr] gap-4 rounded p-4 shadow">
-                {sponsor.logo ? (
-                  <Image src={sponsor.logo} width="56" height="56" />
-                ) : (
-                  <div className="h-[56px] w-[56px] bg-blurple-gradient"></div>
-                )}
-
-                <div className="flex flex-col justify-center">
-                  <span className="c2 !leading-[1.5] text-gray-1">
-                    <FormattedMessage
-                      id="silver_sponsor"
-                      defaultMessage="Silver sponsor"
-                    />
-                  </span>
-
-                  <p className="b3">{sponsor.name}</p>
-                </div>
-              </div>
+              <SponsorCard
+                key={sponsor}
+                sponsor={sponsor}
+                tier="generalHighlighted"
+              />
+            )
+          })}
+          {sponsors.general.map((sponsor) => {
+            return (
+              <SponsorCard key={sponsor} sponsor={sponsor} tier="general" />
             )
           })}
         </div>
@@ -48,3 +55,36 @@ function Sponsors() {
 }
 
 export default Sponsors
+
+const SponsorCard = ({ sponsor, tier }) => {
+  return (
+    <div
+      className={classnames(
+        "grid grid-cols-[auto_1fr] gap-4 rounded p-4 shadow",
+        tier === "generalHighlighted" && "bg-gray-3"
+      )}
+    >
+      {tier === "silver" &&
+        (sponsor.logo ? (
+          <Image src={sponsor.logo} width="56" height="56" />
+        ) : (
+          <div className="h-[56px] w-[56px] bg-blurple-gradient" />
+        ))}
+
+      <div className="flex flex-col justify-center">
+        <span className="c2 !leading-[1.5] text-gray-1">
+          {tier === "silver" ? (
+            <FormattedMessage
+              id="silver_sponsor"
+              defaultMessage="Silver sponsor"
+            />
+          ) : (
+            <FormattedMessage id="sponsor" defaultMessage="Sponsor" />
+          )}
+        </span>
+
+        <p className="b3">{sponsor.name || sponsor}</p>
+      </div>
+    </div>
+  )
+}
