1c721c64234afd32029d4ca85ec54aadbd3c3a4a
Mika Busante <mika@mikabusante.com>
2022-08-04 15:32:02 -0400

    edit text alignment on centered TwoUpFeature, rel #73

--- components/TwoUpFeature.tsx
+++ components/TwoUpFeature.tsx
@@ -14,19 +14,21 @@ export type TwoUpFeatureProps = {
 /** Two-column (on desktop) feature section */
 const TwoUpFeature = ({ features, padding }: TwoUpFeatureProps) => {
   return (
-    <div className="gap-gutter md:flex">
+    <div className="mb-32 gap-gutter lg:mb-0 lg:flex">
       {features.map(({ icon, title, copy, cta, cta_link }) => (
         <div
-          className={`grid py-8 text-center md:py-32 lg:grid-cols-6 ${padding}`}
+          className={`grid justify-items-center py-8 text-center lg:grid-cols-6 lg:py-32 ${padding}`}
           key={icon}
         >
-          <div className="flex flex-col items-center md:pie-4 lg:col-span-4 lg:col-start-2 lg:pie-0">
-            <SVG
-              src={icon}
-              className="-ml-2 h-auto w-20 text-accent-blurple md:-ml-4 md:w-32"
-            />
-            <h2 className="h4 mb-5 mt-4">{title}</h2>
-            <h2 className="sh1 mb-8">{copy}</h2>
+          <SVG
+            src={icon}
+            className="-ml-2 h-auto w-20 text-accent-blurple md:-ml-4 md:w-32 lg:col-span-6"
+          />
+          <h2 className="h4 mb-5 mt-4 lg:col-span-6">{title}</h2>
+          <h2 className="sh1 mb-8 lg:col-span-6 lg:col-start-1 xl:col-span-4 xl:col-start-2">
+            {copy}
+          </h2>
+          <div className="lg:col-span-6">
             <LinkButton size="large" href={cta_link}>
               {cta}
             </LinkButton>
