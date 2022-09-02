ee983e1f927b506ee6a657c94d1b2ee294d63768
Daniel Gamage <hello@danielgamage.com>
2022-09-02 15:22:39 -0400

    adding missing `b1` default style to basic pages

--- components/BasicPage.tsx
+++ components/BasicPage.tsx
@@ -5,7 +5,7 @@
  */
 export const BasicPage = ({ children }) => {
   return (
-    <div className="grid-cols-12 pt-[var(--header-area)] md:grid">
+    <div className="grid-cols-12 pt-[var(--header-area)] md:grid b1">
       <div className="pt-16 md:col-span-10 md:col-start-2 lg:col-span-8 lg:col-start-3">
         {children}
       </div>
