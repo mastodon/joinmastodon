b395a1645f80eb53c9f6f52146f38a11a62c0592
Eugen Rochko <eugen@zeonfederated.com>
2022-10-20 15:45:06 +0200

    Add canonical link tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -35,8 +35,10 @@ export const Layout = ({
         <meta property="twitter:card" content="summary_large_image" />
 
         {locales.filter(locale => locale.code !== currentLocale).map(locale => (
-          <link key={locale.code} rel="alternate" hrefLang={locale.code} href={`${BASE_URL}/${locale.code === defaultLocale ? "" : locale.code}${currentPath}`} />
+          <link key={locale.code} rel="alternate" hrefLang={locale.code} href={`${BASE_URL}${locale.code === defaultLocale ? "" : `/${locale.code}`}${currentPath}`} />
         ))}
+
+        <link rel="canonical" href={`${BASE_URL}${currentLocale === defaultLocale ? "" : `/${currentLocale}`}${currentPath}`} />
       </Head>
     </div>
   )
