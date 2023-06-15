48caa70739ebada6f60320a00191e616c8e34527
lamemakes <wes@lamemakes.com>
2023-06-14 22:44:37 -0400

    Added space

--- types/api.ts
+++ types/api.ts
@@ -1,4 +1,4 @@
-export type ApiPaths = 'jobs' | 'categories' | 'languages'| 'servers' | 'statistics'
+export type ApiPaths = 'jobs' | 'categories' | 'languages' | 'servers' | 'statistics'
 
 export type Category = {
   category: string
