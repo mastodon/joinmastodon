0af5d10999f4c4c9e1d2b7c87d2064bfdb02c337
Michael Cohen <michael.lee.cohen@gmail.com>
2024-10-29 08:49:33 -0700

    Update donor policies (#797)

---
+++ pages/donor-policy/de/mastodon-ggmbh.tsx
@@ -0,0 +1,103 @@
+import Image from "next/legacy/image"
+
+import Link from "next/link"
+import Head from "next/head";
+
+import Hero from "../../../components/Hero"
+import Layout from "../../../components/Layout"
+import heroImage from "../../../public/illustrations/apps_hero_desktop.png"
+import ukFlagIcon from "../../../public/uk_flag_icon_round.svg"
+import PolicyPage from "../../../components/PolicyPage";
+
+const MastodonGGmbHDonorPolicy = () => (
+    <Layout>
+      <div dir="ltr" className="[unicode-bidi:plaintext]">
+        <Hero desktopImage={heroImage} mobileImage={heroImage}>
+          <h1 className="h2 mb-4" lang="de">Mastodon gGmbH Spendenannahme-Richtlinie</h1>
+          <p className="sh1" lang="de">Zuletzt aktualisiert: 14. Oktober 2024</p>
+        </Hero>
+      </div>
+
+      <PolicyPage>
+        <section lang="de">
+          <p className="mt-4 flex justify-end pb-2">
+            <Link
+              className="flex content-center gap-x-1"
+              href="/donor-policy/mastodon-ggmbh"
+            >
+              <span className="hover:text-blurple-600">English translation</span>
+              <Image 
+                src={ukFlagIcon}
+                className="aspect-square"
+                width="20"
+                height="20"
+                alt="Englische Übersetzung"
+              />
+            </Link>
+          </p>
+          <p className="mt-4">
+            Mastodons Vision ist es, die Social Media Landschaft neu zu gestalten. Dazu gehört Inklusivität, Vielfältigkeit
+            und Nutzerorientierung sowie die Förderung des Dialogs. Unser Mission ist es, zentrale Plattformen durch robuste
+            Social-Networking-Software zu ersetzen, die von Natur aus dezentralisiert, quelloffen und vollständig interoperabel
+            sind – mit einem klaren Fokus auf den Schutz der Privatsphäre.
+          </p>
+          <p className="mt-4">
+            Diese Aufgabe wird zum Teil durch Spenden von Einzelpersonen und Organisationen aus aller Welt ermöglicht. Mit
+            dieser Richtlinie definiert Mastodon die wesentlichen Voraussetzungen für die Annahme und Anerkennung von Spenden
+            und Zuwendungen fest, um unsere Mission zu fördern.
+          </p>
+          <p className="mt-4">
+            Alle finanziellen Mittel, die die Mastodon gGmbH erhält, müssen im Einklang mit unserer Mission und unseren Zielen
+            stehen. Die Wahrung unserer Unabhängigkeit hat dabei höchste Priorität.
+          </p>
+          <p className="mt-4">
+            Um dies sicherzustellen, definieren wir im Folgenden die Kriterien für die Ablehnung jeglicher Unterstützung, die
+            den Werten von Mastodon widerspricht oder unsere Mission, unser Team, unsere Integrität oder die Organisation als
+            Ganzes gefährden könnte:
+          </p>
+          <ul className="mt-4 list-disc ml-8">
+            <li className="mt-2">Hersteller von Waffen und militärische Auftragnehmer</li>
+            <li className="mt-2">Jegliche Unternehmen, die sich an illegalen Aktivitäten und Menschenrechtsverletzungen beteiligen</li>
+            <li className="mt-2">Jegliche Unternehmen, die das Team, die Unabhängigkeit und die Gemeinschaften der Organisation gefährden</li>
+            <li className="mt-2">
+              Unternehmen, die das Ansehen der Organisation gefährden, einschließlich, aber nicht ausschließlich, jugendfreie Webseiten,
+              Affiliate- und Bewertungsseiten, Online-Wetten und Glücksspiel, SEO-Dienstleistungen, Social-Media-Einkauf und andere
+            </li>
+            <li className="mt-2">Jegliche Aussagen oder Handlungen, die die Organisation dazu veranlassen, die Zweckmäßigkeit der Annahme einer Spende in Frage zu stellen</li>
+          </ul>
+          <p className="mt-4">
+            Die Mastodon gGmbH nimmt die Einhaltung dieser Spendenrichtlinien sehr ernst. Finanzierungen sind entscheidend, um das
+            Wachstum der von Mastodon gGmbH zu unterstützen, dürfen jedoch nicht die Werte der Organisation gefährden. Entscheidungen
+            werden nach Prüfung jeder GeldgeberIn und jeder Finanzierungsquelle getroffen. Wenn eine SpenderIn oder ein Unternehmen
+            nicht den oben genannten Kriterien entspricht, wird die Spende innerhalb von 30 Tagen über die ursprüngliche Zahlungsmethode,
+            sofern möglich, zurückerstattet.
+          </p>
+        </section>
+        <section lang="de" className="mt-8">
+          <h4 className="h4 mb-6">Steuerliche Konsequenzen</h4>
+          <p>
+            Mastodon gGmbH ist nicht in der Lage, potenziellen Spendern steuerliche Beratung zu bieten. SpenderInnen sollten sich
+            bezüglich der steuerlichen Abzugsfähigkeit von Spenden an einen Steuerberater wenden. Die Organisation wurde 2021 offiziell
+            als gGmbH (eine deutsche Form der gemeinnützigen GmbH) eingetragen.
+          </p>
+        </section>
+        <section lang="de" className="mt-8">
+          <h4 className="h4 mb-6">Annahme von Sachspenden</h4>
+          <p>
+            Sachspenden dürfen nur vom Geschäftsführer oder einem schriftlich vom Geschäftsführer beauftragten MitarbeiterIn mit der
+            Befugnis zur Annahme solcher Spenden angenommen werden.
+          </p>
+        </section>
+
+        <Head>
+          <title lang="de">Mastodon gGmbH Spendenannahme-Richtlinie - Mastodon</title>
+          <meta
+            property="og:title"
+            content="Mastodon gGmbH Spendenannahme-Richtlinie"
+          />
+        </Head>
+      </PolicyPage>
+    </Layout>
+);
+
+export default MastodonGGmbHDonorPolicy;
--- pages/donor-policy/mastodon-inc.tsx
+++ pages/donor-policy/mastodon-inc.tsx
@@ -8,17 +8,17 @@ const MastodonIncDonorPolicy = () => (
     <Layout>
       <div dir="ltr" className="[unicode-bidi:plaintext]">
         <Hero desktopImage={heroImage} mobileImage={heroImage}>
-          <h1 className="h2 mb-4">Mastodon, Inc. Donor Policy</h1>
-          <p className="sh1">Last updated September 16, 2024</p>
+          <h1 lang="en-US" className="h2 mb-4">Mastodon, Inc. Donor Policy</h1>
+          <p lang="en-US" className="sh1">Last updated October 14, 2024</p>
         </Hero>
       </div>
 
       <PolicyPage>
-        <section>
+        <section lang="en-US">
           <p className="mt-4">
-            Mastodon, Inc’s vision is to reimagine the social media landscape, replacing centralized platforms with robust
-            social networking software that is inherently decentralized, open source, fully interoperable, with a commitment
-            to privacy. 
+            Mastodon, Inc’s vision is to reimagine the social media landscape, one that is inclusive, diverse, user driven
+            and supports dialogue. Our mission is to replace centralised platforms with robust social networking software
+            that is inherently decentralised, open source and fully interoperable, with a commitment to privacy. 
           </p>
           <p className="mt-4">
             This work is made possible, in part, by donations made by individuals and organizations from around the world.
@@ -26,8 +26,8 @@ const MastodonIncDonorPolicy = () => (
             and grants to further its mission.
           </p>
           <p className="mt-4">
-            Any funding towards Mastodon, Inc. must be consistent with the organization’s stated mission and purposes, and
-            which prioritizes its independence.
+            Any funding towards Mastodon, Inc. must be consistent with the organization’s stated mission and purposes which
+            prioritizes its independence.
           </p>
           <p className="mt-4">
             To ensure this, below is an outline of the criteria to reject any form of support that is counter to Mastodon’s
@@ -40,14 +40,14 @@ const MastodonIncDonorPolicy = () => (
             <li className="mt-2">Any other statements or actions that causes the organization to question the advisability of accepting a contribution</li>
           </ul>
           <p className="mt-4">
-            Mastodon, Inc. takes its funding sources very seriously. Funding is essential to adequately support its growth,
-            but will not be allowed to compromise its values. Decisions are taken after thorough examination of each funder
-            and funding source. Such decisions may also be referred to the Board of Directors for further exploration and a
-            final decision. If a donor does not meet the above criteria, they will be refunded within 30 days of their donation
+            Mastodon, Inc. takes compliance and origin of funding sources very seriously. Funding is essential to adequately
+            support its growth, but will not be allowed to compromise its values. Decisions are taken after thorough examination
+            of each funder and funding source. Such decisions may also be referred to the Board of Directors for further exploration
+            and a final decision. If a donor does not meet the above criteria, they will be refunded within 30 days of their donation
             via the original payment method where possible.
           </p>
         </section>
-        <section  className="mt-8">
+        <section lang="en-US" className="mt-8">
           <h4 className="h4 mb-6">Tax Consequences</h4>
           <p>
             Mastodon, Inc. is unable to offer tax advice to would-be donors. Donors who are concerned about tax-deductibility of
@@ -55,7 +55,7 @@ const MastodonIncDonorPolicy = () => (
             under United States law as a Delaware corporation.
           </p>
         </section>
-        <section className="mt-8">
+        <section lang="en-US" className="mt-8">
           <h4 className="h4 mb-6">Accepting Gifts-in-Kind</h4>
           <p>
           Gifts-in-kind may only be accepted by the Executive Director or by an officer delegated in writing by the Executive Director
@@ -64,7 +64,7 @@ const MastodonIncDonorPolicy = () => (
         </section>
 
         <Head>
-          <title>Mastodon, Inc. Donor Policy - Mastodon</title>
+          <title lang="en-US">Mastodon, Inc. Donor Policy - Mastodon</title>
           <meta
             property="og:title"
             content="Mastodon, Inc. Donor Policy"
