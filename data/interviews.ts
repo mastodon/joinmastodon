3f8b2dd820a9fde0f5aa83b9584d482eb825d588
Andy Piper <andypiper@users.noreply.github.com>
2025-01-16 15:49:05 +0000

    Add recent podcasts and news items to the About page (#848)
    * Update podcast coverage.
    * Update press coverage.
    
    Signed-off-by: Andy Piper <andypiper@users.noreply.github.com>

--- data/interviews.ts
+++ data/interviews.ts
@@ -1,5 +1,33 @@
 const interviews = [
   {
+    url: "https://techcrunch.com/podcast/social-media-should-be-built-on-protocols-not-platforms-says-mastodon-ceo-eugen-rochko/",
+    title: "Social media should be built on protocols, not platforms says Mastodon CEO Eugen Rochko",
+    show: "Equity",
+    date: "2025-01-15",
+    icon: require("../public/press/equity.png"),
+  },
+  {
+    url: "https://hackaday.com/2024/10/16/floss-weekly-episode-805-mastodon-bring-your-own-algorithm/",
+    title: "Mastodon — Bring Your Own Algorithm",
+    show: "FLOSS Weekly",
+    date: "2024-10-16",
+    icon: require("../public/press/floss-weekly.jpg"),
+  },
+  {
+    url: "https://softwareengineeringdaily.com/2024/01/31/mastodon-with-eugen-rochko/",
+    title: "Mastodon with Eugen Rochko",
+    show: "Software Engineering Daily",
+    date: "2024-01-31",
+    icon: require("../public/press/sed.png"),
+  },
+  {
+    url: "https://dot-social.simplecast.com/episodes/eugen-rochko",
+    title: "The State of the Federation, with Mastodon's Eugen Rochko",
+    show: "dotSocial",
+    date: "2023-12-20",
+    icon: require("../public/press/dotsocial.jpg"),
+  },
+  {
     url: "https://www.theverge.com/23658648/mastodon-ceo-twitter-interview-elon-musk-twitter",
     title: "Can Mastodon seize the moment from Twitter?",
     show: "Decoder",
