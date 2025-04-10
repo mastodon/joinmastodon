1ec815ec976884daa70d2bc2f106f329df2760b2
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-04-10 16:26:11 +0200

    fix visual glitch with select component

--- components/SelectMenu.tsx
+++ components/SelectMenu.tsx
@@ -1,4 +1,9 @@
-import { Listbox } from "@headlessui/react"
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react"
 import classNames from "classnames"
 import DisclosureArrow from "../public/ui/disclosure-arrow.svg?inline"
 
@@ -27,29 +32,29 @@ export const SelectMenu = ({
   return (
     <Listbox value={value} onChange={onChange}>
       <div className="b3 inline-flex w-full sm:w-auto">
-        <div className="relative w-full sm:w-auto">
-          <Listbox.Button className="relative w-full cursor-pointer rounded-md border border-gray-3 py-4 pl-4 pr-10 text-left focus:outline-none focus:ring-1 focus:ring-blurple-500 sm:w-auto">
+        <div className="w-full relative">
+          <ListboxButton className="flex w-full items-center gap-2 cursor-pointer rounded-md border border-gray-3 p-4 text-left focus:outline-none focus:ring-1 focus:ring-blurple-500 sm:w-auto">
             <span className="block truncate text-gray-1">
               <span className="font-medium">{label}: </span>
               <span className="font-bold">{selectedLabel}</span>
             </span>
 
-            <span className="end-3 pointer-events-none absolute block-start-5">
+            <span className="pointer-events-none">
               <DisclosureArrow
                 className="h-4 w-4 text-gray-2"
                 fill="currentColor"
               />
             </span>
-          </Listbox.Button>
+          </ListboxButton>
 
-          <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full min-w-max overflow-auto rounded-md bg-white py-1 shadow-lg ring-1 ring-gray-3 focus:outline-none">
+          <ListboxOptions className="absolute z-10 mt-1 max-h-56 w-full min-w-max overflow-auto rounded-md bg-white py-1 shadow-lg ring-1 ring-gray-3 focus:outline-none">
             {options.map(({ label: optionLabel, value: optionValue }) => (
-              <Listbox.Option
+              <ListboxOption
                 key={optionValue}
                 value={optionValue}
-                className={({ active }) =>
+                className={({ focus }) =>
                   classNames(
-                    active ? "bg-blurple-500 text-white" : "text-gray-1",
+                    focus ? "bg-blurple-500 text-white" : "text-gray-1",
                     "relative cursor-pointer select-none py-3 px-4 font-medium text-gray-1"
                   )
                 }
@@ -61,9 +66,9 @@ export const SelectMenu = ({
                     </span>
                   </>
                 )}
-              </Listbox.Option>
+              </ListboxOption>
             ))}
-          </Listbox.Options>
+          </ListboxOptions>
         </div>
       </div>
     </Listbox>
