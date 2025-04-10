63bdc9a47238657dd24acfd604cea9357f5b602c
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-04-10 18:08:48 +0200

    break out new Button component and utilize defineMessages for intl

---
+++ components/Button.tsx
@@ -0,0 +1,33 @@
+import {
+  Button as HeadlessButton,
+  ButtonProps as HeadlessButtonProps,
+} from "@headlessui/react"
+import classNames from "classnames"
+
+type ButtonProps = HeadlessButtonProps & {
+  dark?: boolean
+}
+
+export function Button({
+  children,
+  className,
+  dark = false,
+  ...props
+}: React.PropsWithChildren<ButtonProps>) {
+  return (
+    <HeadlessButton
+      {...props}
+      className={classNames(
+        className,
+        "w-full p-2 flex gap-2 items-center justify-center rounded-md",
+        "text-center font-semibold transition-colors focus:outline-none border-2",
+        !dark &&
+          "bg-white hocus:bg-blurple-600 border-blurple-500 hocus:border-blurple-600 text-blurple-500 hocus:text-white",
+        dark &&
+          "bg-blurple-500 hocus:bg-blurple-600 border-[transparent] text-white"
+      )}
+    >
+      {children}
+    </HeadlessButton>
+  )
+}
