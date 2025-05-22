a6897d2e2e6b3cfce08298ce6aac6a2adf1fe8ad
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-05-22 16:43:43 +0200

    update i18n strings

--- pages/donate/complete.tsx
+++ pages/donate/complete.tsx
@@ -18,13 +18,13 @@ export default function DonateCompletePage() {
     <div className="flex flex-col p-8 gap-2">
       <h1 className="sh1">
         <FormattedMessage
-          id="donate.success.header"
+          id="donate_widget.success.header"
           defaultMessage="Thank you for your contribution!"
         />
       </h1>
       <p className="text-gray-1">
         <FormattedMessage
-          id="donate.success.subtitle"
+          id="donate_widget.success.subtitle"
           defaultMessage="You should receive an email confirming your donation soon."
         />
       </p>
@@ -34,7 +34,10 @@ export default function DonateCompletePage() {
         className="w-full max-w-80 mx-auto my-4"
       />
       <Button onClick={handleClose} fullWidth dark>
-        <FormattedMessage id="donate.success.close" defaultMessage="Close" />
+        <FormattedMessage
+          id="donate_widget.success.close"
+          defaultMessage="Close"
+        />
       </Button>
     </div>
   )
