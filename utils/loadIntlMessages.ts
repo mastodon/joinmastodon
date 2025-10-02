9077ab8982dc29139bad6be58b61c4f9e4f84f79
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-10-02 11:41:29 +0200

    add types to default static props function

--- utils/loadIntlMessages.ts
+++ utils/loadIntlMessages.ts
@@ -1,7 +1,11 @@
 import fs from "fs/promises"
+import { GetStaticPropsContext } from "next"
 import path from "path"
 
-export default async function loadI18nMessages({ locale, defaultLocale }) {
+export default async function loadI18nMessages({
+  locale,
+  defaultLocale,
+}: GetStaticPropsContext) {
   // If the default locale is being used we can skip it
   if (locale === defaultLocale) {
     return {}
@@ -11,7 +15,7 @@ export default async function loadI18nMessages({ locale, defaultLocale }) {
     const languagePath = path.join(process.cwd(), `locales/${locale}.json`)
     try {
       const contents = await fs.readFile(languagePath, "utf-8")
-      return JSON.parse(contents)
+      return JSON.parse(contents) as Record<string, string>
     } catch (error) {
       console.error(error)
     }
