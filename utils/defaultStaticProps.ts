706bce15344b1d8993f3e5524703d6e2fe473798
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-10-02 11:41:29 +0200

    add types to default static props function

--- utils/defaultStaticProps.ts
+++ utils/defaultStaticProps.ts
@@ -1,17 +1,27 @@
+import { GetStaticProps, GetStaticPropsContext } from "next"
 import loadIntlMessages from "./loadIntlMessages"
 
-export function withDefaultStaticProps(origin?) {
-  return async (ctx) => {
-    const {
-      props: originProps = {},
-      revalidate = undefined,
-      ...originRemaining
-    } = origin ? await origin(ctx) : {}
+type ReturnProps<Props extends Record<string, any>> = Props & {
+  intlMessages: Record<string, string>
+}
+
+export function withDefaultStaticProps<
+  Props extends Record<string, any> = Record<string, any>,
+>(origin?: GetStaticProps<Props>): GetStaticProps<ReturnProps<Props>> {
+  return async (ctx: GetStaticPropsContext<ReturnProps<Props>>) => {
+    const { revalidate = 300, ...originResult } = origin
+      ? await origin(ctx)
+      : {}
+    const originProps =
+      "props" in originResult ? originResult.props : ({} as Props)
 
     return {
-      props: { intlMessages: await loadIntlMessages(ctx), ...originProps },
-      revalidate: revalidate || 300,
-      ...originRemaining,
+      ...originResult,
+      props: {
+        ...(originProps as Props),
+        intlMessages: await loadIntlMessages(ctx),
+      },
+      revalidate: revalidate,
     }
   }
 }
